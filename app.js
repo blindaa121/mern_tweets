dea875670e427d46fef3a83bd7e05e94807966b0
Bryan <blindaa121@gmail.com>
2021-09-18 11:33:45 -0700

    Setup users and tweets express routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express() // Creates a new Express server
 const port = process.env.PORT || 5000 // Heroku will use process.env.PORT else local 5000 on development
 const mongoose = require('mongoose') // Mongoose acts as an ORM 
 const db = require('./config/keys').mongoURI // MongoDB database
+const users = require('./routes/api/users') // Users api
+const tweets = require('./routes/api/tweets') // Tweets api
 
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB successfully'))
@@ -10,5 +12,5 @@ mongoose.connect(db, { useNewUrlParser: true })
 
 app.get('/', (req, res) => res.send('Making a change')) // Set up GET route 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
-
-
+app.use('/api/users', users)
+app.use('/api/tweets', tweets)
