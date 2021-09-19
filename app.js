29d0a815ea6cca90d3b2512c47979dfdcae7425d
Bryan <blindaa121@gmail.com>
2021-09-19 09:48:48 -0700

    Set up user register api

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const mongoose = require('mongoose') // Mongoose acts as an ORM
 const db = require('./config/keys').mongoURI // MongoDB database
 const users = require('./routes/api/users') // Users api
 const tweets = require('./routes/api/tweets') // Tweets api
+const bodyParser = require('body-parser') // Parse data into JSON 
 
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Making a change')) // Set up GET route 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.get('/', (req, res) => res.send('Whattttup')) // Set up GET route 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
 app.use('/api/users', users)
 app.use('/api/tweets', tweets)
